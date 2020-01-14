6dc486c6112c5c97fe0f8cf3d53b148649c42b63
basri umar <basriumar12@gmail.com>
2020-01-14 17:05:29 +0700

    card and parsing

--- lib/main.dart
+++ lib/main.dart
@@ -10,105 +10,182 @@ void main() {
 class HalamanSatu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return new Scaffold(
       appBar: new AppBar(
-        backgroundColor: Colors.red,
-        leading: new Icon(Icons.home),
-        title: Text("App Bar"),
-        actions: <Widget>[new Icon(Icons.search)],
+        title: new Text("Card and Passing"),
       ),
       body: new Container(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
 
-        //change column to row
+            new CardSaya(icon: Icons.home, teks : "Menu 1", colorIcon: Colors.blue),
+            new CardSaya(icon: Icons.home, teks : "Menu 2", colorIcon: Colors.red),
+            new CardSaya(icon: Icons.home, teks : "Menu 3", colorIcon: Colors.green)
+            // bisa pakai class
+//            new Card(
+//                child: new Column(
+//              children: <Widget>[
+//                new Icon(
+//                  Icons.home,
+//                  size: 70.0,
+//                  color: Colors.red,
+//                ),
+//                new Text(
+//                  "Home",
+//                  style: new TextStyle(fontSize: 20.0),
+//                )
+//              ],
+//            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CardSaya extends StatelessWidget {
+  CardSaya({this.icon, this.teks, this.colorIcon});
+  final IconData icon;
+  final String teks;
+  final Color colorIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Container(
+      padding: new EdgeInsets.all(10.0),
+      child: new Card(
           child: new Column(
         children: <Widget>[
           new Icon(
-            Icons.access_alarm,
+           icon,
             size: 70.0,
-            color: Colors.red,
+            color: colorIcon,
           ),
-          new Row( children: <Widget>[
-            new Icon(
-              Icons.timer,
-              size: 70.0,
-              color: Colors.red,
-            ),
-            new Icon(
-              Icons.account_box,
-              size: 70.0,
-              color: Colors.red,
-            ),
-          ],),
-          new Icon(
-            Icons.timer,
-            size: 70.0,
-            color: Colors.red,
-          ),
-          new Icon(
-            Icons.account_box,
-            size: 70.0,
-            color: Colors.red,
-          ),
+          new Text(
+            teks,
+            style: new TextStyle(fontSize: 20.0),
+          )
         ],
       )),
     );
   }
 }
+
+///*
 //import 'package:flutter/material.dart';
 //
-//void main() => runApp(new myApp());
+//void main() {
+//  runApp(new MaterialApp(
+//    home: new HalamanSatu(),
+//  ));
+//}
 //
-//class myApp extends StatelessWidget {
-//  List<String> data = ["Indonesia", "Araba Saudi", "Malaysia","America","Singapura","Korea","Oman","Jepang"];
-//
+////style text
+//class HalamanSatu extends StatelessWidget {
 //  @override
 //  Widget build(BuildContext context) {
-//    return new MaterialApp(
-//      title: "Makanan",
-//      home: new Scaffold(
-//        appBar: new AppBar(
-//          title: const Text('Bahan Makanan'),
-//        ),
-//        body: ListView.builder(
-//          itemCount: data.length,
-//          itemBuilder: (context, index) {
-//            return ListTile(
-//              title: Text(data[index]),
-//              // Ketika pengguna melakukan tap pada ListTile, pindah ke DetailScreen.
-//              // Selain membuat DetailScreen, data juga dikirimkan
-//              onTap: () {
-//                Navigator.push(
-//                  context,
-//                  MaterialPageRoute(
-//                    builder: (context) => DetailScreen(item: data[index]),
-//                  ),
-//                );
-//              },
-//            );
-//          },
-//        ),
+//    // TODO: implement build
+//    return new Scaffold(
+//      appBar: new AppBar(
+//        backgroundColor: Colors.red,
+//        leading: new Icon(Icons.home),
+//        title: Text("App Bar"),
+//        actions: <Widget>[new Icon(Icons.search)],
 //      ),
+//      body: new Container(
+//
+//        //change column to row
+//          child: new Column(
+//        children: <Widget>[
+//          new Icon(
+//            Icons.access_alarm,
+//            size: 70.0,
+//            color: Colors.red,
+//          ),
+//          new Row( children: <Widget>[
+//            new Icon(
+//              Icons.timer,
+//              size: 70.0,
+//              color: Colors.red,
+//            ),
+//            new Icon(
+//              Icons.account_box,
+//              size: 70.0,
+//              color: Colors.red,
+//            ),
+//          ],),
+//          new Icon(
+//            Icons.timer,
+//            size: 70.0,
+//            color: Colors.red,
+//          ),
+//          new Icon(
+//            Icons.account_box,
+//            size: 70.0,
+//            color: Colors.red,
+//          ),
+//        ],
+//      )),
 //    );
 //  }
 //}
-//
-//class DetailScreen extends StatelessWidget {
-//  DetailScreen({Key key, this.item}) : super(key: key);
-//  final String item;
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      appBar: AppBar(
-//          title: Text('Detail Bahan Makanan'),
-//          leading: IconButton(
-//            icon: Icon(Icons.arrow_back),
-//            onPressed: () => Navigator.pop(context, false),
-//          )),
-//      body: Center(
-//        child: Text(item),
-//      ),
-//    );
-//  }
-//}
+////import 'package:flutter/material.dart';
+////
+////void main() => runApp(new myApp());
+////
+////class myApp extends StatelessWidget {
+////  List<String> data = ["Indonesia", "Araba Saudi", "Malaysia","America","Singapura","Korea","Oman","Jepang"];
+////
+////  @override
+////  Widget build(BuildContext context) {
+////    return new MaterialApp(
+////      title: "Makanan",
+////      home: new Scaffold(
+////        appBar: new AppBar(
+////          title: const Text('Bahan Makanan'),
+////        ),
+////        body: ListView.builder(
+////          itemCount: data.length,
+////          itemBuilder: (context, index) {
+////            return ListTile(
+////              title: Text(data[index]),
+////              // Ketika pengguna melakukan tap pada ListTile, pindah ke DetailScreen.
+////              // Selain membuat DetailScreen, data juga dikirimkan
+////              onTap: () {
+////                Navigator.push(
+////                  context,
+////                  MaterialPageRoute(
+////                    builder: (context) => DetailScreen(item: data[index]),
+////                  ),
+////                );
+////              },
+////            );
+////          },
+////        ),
+////      ),
+////    );
+////  }
+////}
+////
+////class DetailScreen extends StatelessWidget {
+////  DetailScreen({Key key, this.item}) : super(key: key);
+////  final String item;
+////
+////  @override
+////  Widget build(BuildContext context) {
+////    return Scaffold(
+////      appBar: AppBar(
+////          title: Text('Detail Bahan Makanan'),
+////          leading: IconButton(
+////            icon: Icon(Icons.arrow_back),
+////            onPressed: () => Navigator.pop(context, false),
+////          )),
+////      body: Center(
+////        child: Text(item),
+////      ),
+////    );
+////  }
+////}
+//*/
